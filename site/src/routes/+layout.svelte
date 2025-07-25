5dd95761df57a3a7bc86d5552363b67a03356456
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2025-07-25 18:47:33 -0500

    Initialized Frontend

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/daily">Daily View</a>
+	<a href="/plan">Plan</a>
+	<a href="/"></a>
+	<a href="/"></a>
+</nav>
+
+{@render children()}

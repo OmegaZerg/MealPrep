ef6591e3035ef1baabcb0bfa3e012448ddc7e508
OmegaZerg <Ragged17@Bellsouth.net>
2025-07-25 16:26:58 -0500

    Lots and lots of testing performed to get things acting right. Also created a bunch of sample foods and meals. Will still need a few more of those I believe at some point. Have laid out 2 of the tables, foods and meals.

--- backend/constants.py
+++ backend/constants.py
@@ -2,18 +2,52 @@ SAMPLE_FOODS = [
     ("Scrambled Eggs", "Whole eggs with yolk", "Ounce", 4, 169, 13, 2, 12, None),
     ("Toast", "1 Piece with butter", "Slice", 1, 180, 9, 21, 4, None),
     ("Milk", "1 cup of milk", "Ounce", 8, 100, 3, 8, 9, None),
+    ("Turkey Sandwhich", "2 bread, 4 ounces turkey lunchmeat, lettuce, tomato, onion, mustard", None, 1, 360, 7, 40, 36, None),
+    ("Spinach Salad", "Large salad with light italian dressing", None, 1, 140, 5, 21, 5, None),
+    ("Honeycrisp Apple", "Its an apple", "Ounce", 8, 118, 0, 32, 0, None ),
+    ("Orange Juice", "1 cup of orange juice", "Ounch", 8, 117, 1, 25, 2, None),
+    ("Banana", "Its a banana", None, 1, 114, 0, 27, 1, None),
+    ("Plain Oatmeal", "Plain old quaker oats, cooked", "Ounce", 7, 136, 2, 24, 5, None),
+    ("Pure Protein Protein Bar", "Its packed full of flavores and proteins. Deluxe Chocolate.", None, 1, 180, 5, 17, 21, None),
+    ("NY Strip Steak", "Cooked beef ultra premium first cut prime established - grilled (of course)", "Ounce", 7, 310, 12, 0, 46, None),
+    ("Cliff Bar - Chocolate Chip", "Energy Bar that helps you climb mountains..or something", None, 1, 250, 6, 43, 10, None),
+    ("Progresso Chickpea and Noodle Soup", "18.5 ounce can of soup", None, 1, 340, 7, 53, 17, None),
+    ("Collard Greens", "Steamed greens", "gram", 100, 29, 1, 6, 4, None),
+    ("Red Potato", "Plain with salf/pepper", "Ounce", 5, 138, 0, 33, 4, None),
+]
+SAMPLE_MEALS = [
+    ("Lunch 1", "Sandwhich, apple, and salad", "Lunch", "Turkey Sandwhich", "Honeycrisp Apple", "Spinach Salad", None, None),
+    ("Breakfast 1", "Toast, scrambled eggs, and orange juice", "Breakfast", "Scrambled Eggs", "Toast", "Orange Juice", None, None),
+    ("Dinner 1", "Steak, greens, and potatoes", "Dinner", "NY Strip Steak", "Collard Greens", "Red Potato", None, None),
 ]
 CREATE_FOODS_TABLE = """
 CREATE TABLE foods (
     food_name TEXT PRIMARY KEY NOT NULL,
     description TEXT NULL,
-    unit TEXT NOT NULL,
-    quantity INTEGER NOT NULL,
+    unit TEXT NULL,
+    quantity INTEGER NULL,
     calories INTEGER NULL,
     fat INTEGER NULL,
     carbs INTEGER NULL,
     protein INTEGER NULL,
     tag TEXT NULL
+);
+"""
+CREATE_MEALS_TABLE = """
+CREATE TABLE meals (
+    meal_name TEXT PRIMARY KEY NOT NULL,
+    description TEXT NULL,
+    tag TEXT NOT NULL,
+    food_1 TEXT NOT NULL,
+    food_2 TEXT NULL,
+    food_3 TEXT NULL,
+    food_4 TEXT NULL,
+    food_5 TEXT NULL,
+    FOREIGN KEY (food_1) REFERENCES foods (food_name),
+    FOREIGN KEY (food_2) REFERENCES foods (food_name),
+    FOREIGN KEY (food_3) REFERENCES foods (food_name),
+    FOREIGN KEY (food_4) REFERENCES foods (food_name),
+    FOREIGN KEY (food_5) REFERENCES foods (food_name)
 );
 """
 SELECT_ALL_TABLES = """
@@ -22,5 +56,10 @@ SELECT * FROM sqlite_master;
 
 SELECT_ALL_FOODS = """
 SELECT * FROM foods
-ORDER BY food_name DESC;
+ORDER BY food_name ASC;
+"""
+
+SELECT_ALL_MEALS = """
+SELECT * FROM meals
+ORDER BY meal_name ASC;
 """
--- backend/main.py
+++ backend/main.py
@@ -5,27 +5,46 @@ from constants import *
 def main():
     print("Hello from backend!")
     con = sqlite3.connect("mealprep.db")
+    con.execute("PRAGMA foreign_keys = ON")
     cur = con.cursor()
 
     if not table_exists("foods", cur):
         cur.execute(CREATE_FOODS_TABLE)
+        con.commit()
+    if not table_exists("meals", cur):
+        cur.execute(CREATE_MEALS_TABLE)
+        con.commit()
 
     #Even though columns are not required, we must still provide data for every column when inserting. Any values we do not have will required defaults like "N/A"
     tables = cur.execute(SELECT_ALL_TABLES)
     print(tables.fetchall())
-    cur.executemany("INSERT INTO foods VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", SAMPLE_FOODS)
-    con.commit()
+
+    #Testing foods
+    # cur.executemany("INSERT INTO foods VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", SAMPLE_FOODS)
+    # con.commit()
+
+    #Testing meals
+    # cur.executemany("INSERT INTO meals VALUES (?, ?, ?, ?, ?, ?, ?, ?)", SAMPLE_MEALS)
+    # con.commit()
+
+#     query = """
+# DROP TABLE foods;
+# """
+#     cur.execute(query)
+#     con.commit()
 
 #     query = """
 # DELETE FROM foods;
 # """
 #     cur.execute(query)
 #     con.commit()
-
+    print("Displaying all foods: ")
     for row in cur.execute(SELECT_ALL_FOODS):
         print(row)
 
-    #print(cur.execute(SELECT_ALL_FOODS))
+    print("\nDisplaying all meals: ")
+    for row in cur.execute(SELECT_ALL_MEALS):
+        print(row)
 
     con.close()
 

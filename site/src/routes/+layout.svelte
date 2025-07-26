2a1e6102b79396ee535a86c49c9199924cc5a478
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2025-07-26 09:43:00 -0500

    Added Responsive Nav Bar

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -4,12 +4,26 @@
 	let { children } = $props();
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/daily">Daily View</a>
-	<a href="/plan">Plan</a>
-	<a href="/"></a>
-	<a href="/"></a>
+<nav class="w-full bg-orange-600 py-5 md:py-0">
+	<div class="la:px-8 mx-auto max-w-7xl px-4 sm:px-6">
+		<div class="flex h-16 items-center justify-between">
+			<div class="flex flex-1 justify-start">
+				<div class="flex items-baseline justify-start gap-x-4">
+					<a href="/">Home</a>
+					<a href="/daily">Daily View</a>
+					<a href="/plan">Plan</a>
+				</div>
+			</div>
+			<h1 class="px-4 text-xl md:text-4xl lg:text-5xl">Meal Prep</h1>
+			<div class="flex flex-1 justify-end">
+				<div class="flex items-baseline gap-x-4">
+					<a href="/meals">Add Meals</a>
+					<a href="/foods">Add Foods</a>
+					<a href="#" class="invisible">Dummy Link</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </nav>
 
 {@render children()}

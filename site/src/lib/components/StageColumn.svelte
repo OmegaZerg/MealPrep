1501773e5082d9b9c508e3612a42bd0ced9fc283
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2025-07-28 17:36:03 -0500

    Finished Card, Stage Components and plan/+page.svelte

--- site/src/lib/components/StageColumn.svelte
+++ site/src/lib/components/StageColumn.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import Card from '$lib/components/Card.svelte';
 
-	let { title, meals } = $props();
-  let mealsWithTypes = $derived(
-		meals.map((meal) => ({ ...meal, type: 'meal' as const }))
-	);
+	let { title, meals, handleCardClick } = $props();
+	let mealsWithTypes = $derived(meals.map((meal) => ({ ...meal, type: 'meal' as const })));
 </script>
 
-<div class="flex h-full flex-col rounded-lg bg-slate-100 p-4">
-	<div>
+<div class="mx-1 flex h-full flex-col rounded-lg bg-slate-200 p-4">
+	<div class="text-semibold text-3xl text-gray-500">
 		{title}
 	</div>
 	<div>
@@ -16,7 +14,7 @@
 			<p>Add a meal to {title}</p>
 		{:else}
 			{#each mealsWithTypes as data (data.id)}
-				<Card {data} />
+				<Card {data} onAddToStage={handleCardClick} />
 			{/each}
 		{/if}
 	</div>

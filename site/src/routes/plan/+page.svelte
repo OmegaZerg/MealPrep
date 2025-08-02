f9b5c4f4c9769de5feb7ae9faf097ecfac071d98
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2025-08-02 18:33:39 -0500

    Resolved merge conflict

--- site/src/routes/plan/+page.svelte
+++ site/src/routes/plan/+page.svelte
@@ -1,56 +1,6 @@
 <script lang="ts">
 	import Stage from '$lib/components/Stage.svelte';
 	import Card from '$lib/components/Card.svelte';
-<<<<<<< HEAD
-
-	interface CardData {
-		name: string;
-		description: string;
-		quantity?: number;
-		unit?: string;
-		type: 'food' | 'meal';
-		tag?: 'breakfast' | 'lunch' | 'dinner';
-		ingredients?: string[];
-		calories: number;
-		carbs: number;
-		fat: number;
-		protein: number;
-	}
-
-	// Test data, would use fetch from db
-	let mealsToShow: CardData[] = $state([
-		{
-			type: 'meal',
-			name: 'breakfast',
-			description: 'food',
-			tag: 'breakfast',
-			calories: 200,
-			carbs: 40,
-			fat: 10,
-			protein: 10
-		},
-		{
-			type: 'meal',
-			name: 'lunch',
-			description: 'food',
-			tag: 'lunch',
-			calories: 200,
-			carbs: 40,
-			fat: 10,
-			protein: 10
-		},
-		{
-			type: 'meal',
-			name: 'dinner',
-			description: 'food',
-			tag: 'dinner',
-			calories: 200,
-			carbs: 40,
-			fat: 10,
-			protein: 10
-		}
-	]);
-=======
 	import type { CardData } from '$lib/types';
 	import { MOCK_DATA } from '$lib/tests';
 
@@ -70,7 +20,6 @@
 
 	// Test data, would use fetch from db
 	let mealsToShow: CardData[] = $state(MOCK_DATA);
->>>>>>> jon
 
 	let breakfasts = $state<CardData[]>([]);
 	let lunches = $state<CardData[]>([]);
@@ -108,11 +57,7 @@
 
 <Stage {breakfasts} {lunches} {dinners} handleCardClick={handleAddToStage} />
 
-<<<<<<< HEAD
-<div class="mx-5 grid grid-cols-2 md:grid-cols-4">
-=======
 <div class="mx-5 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6">
->>>>>>> jon
 	<button onclick={() => (mealsFilter = 'all')}>All</button>
 	<button onclick={() => (mealsFilter = 'breakfast')}>Breakfast</button>
 	<button onclick={() => (mealsFilter = 'lunch')}>Lunch</button>
@@ -127,17 +72,11 @@
 </div>
 
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-<<<<<<< HEAD
-	<div class="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] justify-start gap-6">
-		{#each filteredMeals as meal}
-			<Card data={meal} onAddToStage={handleAddToStage} />
-=======
 	<div class="[display:grid] grid-cols-1 justify-items-center gap-4 lg:grid-cols-2 xl:grid-cols-3">
 		{#each filteredMeals as meal}
 			<div class="w-96">
 				<Card data={meal} onAddToStage={handleAddToStage} />
 			</div>
->>>>>>> jon
 		{/each}
 	</div>
 </div>
